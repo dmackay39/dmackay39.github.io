72e566b358b3ab20c796361ab9f965a8ac668d01
Daniel MacKay <dmmackay91@outlook.com>
2024-12-21 15:32:06 +0000

    Use hashrouter rather than browserrouter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router';
 
 function Header() {
     return (
@@ -9,11 +10,13 @@ function Header() {
         <Container fluid>
           <Navbar.Brand href="/">Daniel MacKay</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/cv">CV</Nav.Link>
-            <Nav.Link href="/projects">Software Projects</Nav.Link>
+            <Nav.Link><Link to="/" className='custom-link'>Home</Link></Nav.Link>
+            <Nav.Link><Link to="/cv" className='custom-link'>CV</Link></Nav.Link>
+            <Nav.Link><Link to="/projects" className='custom-link'>Projects</Link></Nav.Link>
             <NavDropdown title="Other Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/grandadssermons">Grandad's Sermons</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/grandadssermons" className='custom-link'>Grandad's Sermons</Link>
+            </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route } from 'react-router'
 import CV from './pages/CV.tsx';
 import GrandadSermons from './pages/GrandadSermons.tsx';
 import Home from './pages/Home.tsx';
@@ -11,7 +11,7 @@ import SoftwareProjects from './pages/SoftwareProjects.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -20,6 +20,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/projects" element={<SoftwareProjects />} />
       </Routes>
     </App>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
